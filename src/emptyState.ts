b8420fed9ee9082625a0b4b040d9ac4f1f4ccdfa
Julian Waller <git@julusian.co.uk>
2024-01-28 02:15:19 +0000

    chore: refactor

---
+++ src/emptyState.ts
@@ -0,0 +1,75 @@
+import { AtemCameraControlState, VideoSharpeningLevel } from './state.js'
+
+export function createEmptyState(cameraId: number): AtemCameraControlState {
+	return {
+		cameraId,
+
+		lens: {
+			focus: 0,
+			// autoFocused: boolean
+			iris: 0,
+			// autoIris: boolean
+			opticalImageStabilisation: false,
+			zoomPosition: 0,
+			zoomSpeed: 0,
+		},
+
+		video: {
+			// videomode: number
+			whiteBalance: [0, 0],
+			// setAutoWhite: boolean
+			// restoreAutoWhite: boolean
+			exposure: 0,
+			// dynamicRangeMode: 0,
+			videoSharpeningLevel: VideoSharpeningLevel.Off,
+			// recordingFormat: [number, number, number, number]
+			// setAutoExpsureMode: number
+			// shutterAngle: 0,
+			shutterSpeed: 0,
+			gain: 0,
+			// iso: 0,
+			// displayLut: [number, boolean]
+			ndFilterStop: 0,
+		},
+
+		display: {
+			colorBarEnable: false,
+		},
+
+		colorCorrection: {
+			liftAdjust: {
+				red: 0,
+				green: 0,
+				blue: 0,
+				luma: 0,
+			},
+			gammaAdjust: {
+				red: 0,
+				green: 0,
+				blue: 0,
+				luma: 0,
+			},
+			gainAdjust: {
+				red: 1,
+				green: 1,
+				blue: 1,
+				luma: 1,
+			},
+			offsetAdjust: {
+				red: 0,
+				green: 0,
+				blue: 0,
+				luma: 0,
+			},
+			contrastAdjust: {
+				pivot: 0.5,
+				adj: 1,
+			},
+			lumaMix: 1,
+			colorAdjust: {
+				hue: 0,
+				saturation: 1,
+			},
+		},
+	}
+}
--- src/stateBuilder.ts
+++ src/stateBuilder.ts
@@ -1,10 +1,11 @@
 import type { Commands } from 'atem-connection'
-import { AtemCameraControlState, VideoSharpeningLevel } from './state.js'
+import type { AtemCameraControlState } from './state.js'
 import { ChangesBuilder } from './stateBuilder/builder.js'
 import { applyVideoCommand } from './stateBuilder/video.js'
 import { applyLensCommand } from './stateBuilder/lens.js'
 import { applyDisplayCommand } from './stateBuilder/display.js'
 import { applyColorCorrectionCommand } from './stateBuilder/colorCorrection.js'
+import { createEmptyState } from './emptyState.js'
 
 export class AtemCameraControlStateBuilder {
 	readonly #states = new Map<number, AtemCameraControlState>()
@@ -13,85 +14,11 @@ export class AtemCameraControlStateBuilder {
 		this.reset(inputCount)
 	}
 
-	static createEmptyState(cameraId: number): AtemCameraControlState {
-		return {
-			cameraId,
-
-			lens: {
-				focus: 0,
-				// autoFocused: boolean
-				iris: 0,
-				// autoIris: boolean
-				opticalImageStabilisation: false,
-				zoomPosition: 0,
-				zoomSpeed: 0,
-			},
-
-			video: {
-				// videomode: number
-				whiteBalance: [0, 0],
-				// setAutoWhite: boolean
-				// restoreAutoWhite: boolean
-				exposure: 0,
-				// dynamicRangeMode: 0,
-				videoSharpeningLevel: VideoSharpeningLevel.Off,
-				// recordingFormat: [number, number, number, number]
-				// setAutoExpsureMode: number
-				// shutterAngle: 0,
-				shutterSpeed: 0,
-				gain: 0,
-				// iso: 0,
-				// displayLut: [number, boolean]
-				ndFilterStop: 0,
-			},
-
-			display: {
-				colorBarEnable: false,
-			},
-
-			colorCorrection: {
-				liftAdjust: {
-					red: 0,
-					green: 0,
-					blue: 0,
-					luma: 0,
-				},
-				gammaAdjust: {
-					red: 0,
-					green: 0,
-					blue: 0,
-					luma: 0,
-				},
-				gainAdjust: {
-					red: 1,
-					green: 1,
-					blue: 1,
-					luma: 1,
-				},
-				offsetAdjust: {
-					red: 0,
-					green: 0,
-					blue: 0,
-					luma: 0,
-				},
-				contrastAdjust: {
-					pivot: 0.5,
-					adj: 1,
-				},
-				lumaMix: 1,
-				colorAdjust: {
-					hue: 0,
-					saturation: 1,
-				},
-			},
-		}
-	}
-
 	reset(inputCount: number): void {
 		this.#states.clear()
 
 		for (let i = 1; i <= inputCount; i++) {
-			this.#states.set(i, AtemCameraControlStateBuilder.createEmptyState(i))
+			this.#states.set(i, createEmptyState(i))
 		}
 	}
 
@@ -105,7 +32,7 @@ export class AtemCameraControlStateBuilder {
 	#getOrCreateCamera(cameraId: number) {
 		let state = this.#states.get(cameraId)
 		if (!state) {
-			state = AtemCameraControlStateBuilder.createEmptyState(cameraId)
+			state = createEmptyState(cameraId)
 			this.#states.set(cameraId, state)
 		}
 		return state
